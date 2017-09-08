f6520f0a75101df81281e4cdbf0c5c7fc528505b
Ezra Freedlander <freedlanderey@vcu.edu>
2017-09-07 21:39:18 -0400

    Cut and Pasted origonal code.

--- MakeChange/MakeChange.cpp
+++ MakeChange/MakeChange.cpp
@@ -6,8 +6,44 @@
 
 using namespace std;
 
+double cost,
+payment,
+change,
+addChange;
+
+const double dollar = 1.00,
+quarter = 0.25,
+dime = 0.10,
+nickel = .05;
+
 int main()
 {
-    return 0;
+	cout << "Please enter the cost of the product: ";
+	cin >> cost;
+
+	cout << "Please enter the amount paid: ";
+	cin >> payment;
+
+	change = payment - cost;
+
+	while (change < 0)
+	{
+		cout << "That is not enough! You need " << (change * -1) << " more." << endl;
+
+		cout << "Please enter additional amount: ";
+		cin >> addChange;
+
+		change += addChange;
+	}
+
+	payment += addChange;
+
+	cout << "Item Price - $" << cost << endl;
+	cout << "Amount Paid - $" << payment << endl;
+	cout << "Change Due - $" << change << endl;
+
+	cout << "Your Change: " << endl;
+
+	return 0;
 }
 
