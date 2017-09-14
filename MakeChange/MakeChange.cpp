461ff6fe70e6744477afad6978466808087bfd02
Ezra Freedlander <freedlanderey@vcu.edu>
2017-09-13 20:22:35 -0400

    Working on fixing bugs...

--- MakeChange/MakeChange.cpp
+++ MakeChange/MakeChange.cpp
@@ -6,108 +6,109 @@
 
 using namespace std;
 
-float payment,
-change,
-addChange,
-cost;
+float payment;
+float change;
+float addChange;
+float cost;
 
 int testInput,
 amtDollars,
 amtQuarters,
-amtDimes, 
+amtDimes,
 amtNickels;
 
-const float dollar = 1.00,
-quarter = 0.25,
-dime = 0.10,
-nickel = .05;
+const float dollar = 1.00f;
+const float quarter = 0.25f;
+const float dime = 0.10f;
+const float nickel = .05f;
 
 int main()
 {
-	cout << "Please enter the cost of the product: ";
-	cin >> cost;
-	cout << endl;
-
-	testInput = cost * 100;
-
-	//Testing my testInput
-	//cout << "Test input: " << testInput << endl;
-
-	while (testInput % 5 != 0)
-	{
-		cout << "Please input number divisible by .05: ";
+		cout << "Please enter the cost of the product: ";
 		cin >> cost;
 		cout << endl;
 
-		testInput = cost * 100;
-	}
+		testInput = round(cost * 100);
 
-	cout << "Please enter the amount paid: ";
-	cin >> payment;
-	cout << endl;
+		//Testing my testInput
+		cout << "Test input: " << testInput << endl;
 
-	testInput = payment * 100;
+		while (testInput % 5 != 0)
+		{
+			cout << "Please input number divisible by .05: ";
+			cin >> cost;
+			cost = cost;
+			cout << endl;
 
-	while (testInput % 5 != 0)
-	{
-		cout << "Please input number divisible by .05: ";
+			testInput = round(cost * 100);
+		}
+
+		cout << "Please enter the amount paid: ";
 		cin >> payment;
 		cout << endl;
 
-		testInput = payment * 100;
-	}
+		testInput = round(payment * 100);
 
-	change = payment - cost;
+		while (testInput % 5 != 0)
+		{
+			cout << "Please input number divisible by .05: ";
+			cin >> payment;
+			cout << endl;
 
-	while (change < 0)
-	{
-		cout << "That is not enough! You need " << (change * -1) << " more." << endl;
+			testInput = round(payment * 100);
+		}
 
-		cout << "Please enter additional amount: ";
-		cin >> addChange;
+		change = payment - cost;
 
-		change += addChange;
-	}
+		while (change < 0)
+		{
+			cout << "That is not enough! You need " << (change * -1) << " more." << endl;
 
-	payment += addChange;
+			cout << "Please enter additional amount: ";
+			cin >> addChange;
 
-	cout << "Item Price - $" << cost << endl;
-	cout << "Amount Paid - $" << payment << endl;
-	cout << "Change Due - $" << change << endl << endl;
+			change += addChange;
+		}
 
-	cout << "Your Change: " << endl;
+		payment += addChange;
 
-	if (change == 0)
-		cout << "No change." << endl;
+		cout << "Item Price - $" << cost << endl;
+		cout << "Amount Paid - $" << payment << endl;
+		cout << "Change Due - $" << change << endl << endl;
 
-	if (change >= 1)
-	{
-		amtDollars = change / dollar;
-		change = change -(dollar * amtDollars);
-		cout << "Dollars - " << amtDollars << endl;
-	}
+		cout << "Your Change: " << endl;
 
-	if (change >= .25)
-	{
-		amtQuarters = change / quarter;
-		change = change - (quarter * amtQuarters);
-		cout << "Quarters - " << amtQuarters << endl;
-	}
+		if (change == 0)
+			cout << "No change." << endl;
 
-	if (change >= .1)
-	{
-		amtDimes = change / dime;
-		change = change - (dime * amtDimes);
-		cout << "Dimes - " << amtDimes << endl;
-	}
+		if (change >= 1)
+		{
+			amtDollars = change / dollar;
+			change = change - (dollar * amtDollars);
+			cout << "Dollars - " << amtDollars << endl;
+		}
 
-	if (change >= .05)
-	{
-		amtNickels = change / nickel;
-		change = change - (nickel * amtNickels);
-		cout << "Nickles - " << amtNickels << endl;
-	}
+		if (change >= .25)
+		{
+			amtQuarters = change / quarter;
+			change = change - (quarter * amtQuarters);
+			cout << "Quarters - " << amtQuarters << endl;
+		}
 
-	return 0;
+		if (change >= .1)
+		{
+			amtDimes = change / dime;
+			change = change - (dime * amtDimes);
+			cout << "Dimes - " << amtDimes << endl;
+		}
+
+		if (change >= .05)
+		{
+			amtNickels = change / nickel;
+			change = change - (nickel * amtNickels);
+			cout << "Nickels - " << amtNickels << endl;
+		}
+		return 0;
+	
 }
 
